9882c9d5971efa56c667f2d61797ab373d38bcf8
IQ-tree <desbey1@gmail.com>
2020-08-06 22:17:28 +0900

    #1 KJL 0.0.1.js
    Add readme.md

--- kjl/kjl-0.0.1.js
+++ kjl/kjl-0.0.1.js
@@ -1,6 +1,6 @@
 /*! KJL_ v0.0.1 | MIT License */
 /* Used js-cookie library. */
-
+    
 /*! js-cookie v3.0.0-rc.0 | MIT */
 !function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):(e=e||self,function(){var r=e.Cookies,n=e.Cookies=t();n.noConflict=function(){return e.Cookies=r,n}}())}(this,function(){"use strict";function e(e){for(var t=1;t<arguments.length;t++){var r=arguments[t];for(var n in r)e[n]=r[n]}return e}var t={read:function(e){return e.replace(/%3B/g,";")},write:function(e){return e.replace(/;/g,"%3B")}};return function r(n,i){function o(r,o,u){if("undefined"!=typeof document){"number"==typeof(u=e({},i,u)).expires&&(u.expires=new Date(Date.now()+864e5*u.expires)),u.expires&&(u.expires=u.expires.toUTCString()),r=t.write(r).replace(/=/g,"%3D"),o=n.write(String(o),r);var c="";for(var f in u)u[f]&&(c+="; "+f,!0!==u[f]&&(c+="="+u[f].split(";")[0]));return document.cookie=r+"="+o+c}}return Object.create({set:o,get:function(e){if("undefined"!=typeof document&&(!arguments.length||e)){for(var r=document.cookie?document.cookie.split("; "):[],i={},o=0;o<r.length;o++){var u=r[o].split("="),c=u.slice(1).join("="),f=t.read(u[0]).replace(/%3D/g,"=");if(i[f]=n.read(c,f),e===f)break}return e?i[e]:i}},remove:function(t,r){o(t,"",e({},r,{expires:-1}))},withAttributes:function(t){return r(this.converter,e({},this.attributes,t))},withConverter:function(t){return r(e({},this.converter,t),this.attributes)}},{attributes:{value:Object.freeze(i)},converter:{value:Object.freeze(n)}})}(t,{path:"/"})});
 /* js-cookie end */
@@ -9,12 +9,12 @@ const kjlobject = {
     css: function(el, name, val) {
         if (el && name && val) {
             if (el.startsWith("#")) {
-                var newEl = document.getElementById(el.substring(el.indexOf("#")));
+                var newEl = document.getElementById(el.substring(el.indexOf("#")+1, el.length));
                 newEl.style[name] = val;
             }
             else if (el.startsWith(".")) {
-                var classList = document.getElementsByClassName(el.substring(el.indexOf("."))); //type:array
-                for (i = 0; i < classList.length-1; i++) {
+                var classList = document.getElementsByClassName(el.substring(el.indexOf(".")+1, el.length)); //type:array
+                for (i = 0; i < classList.length; i++) {
                     classList[i].style[name] += val;
                 }
             }
@@ -34,8 +34,14 @@ const kjlobject = {
         if (abbv && name && value) {
             if (abbv != 'set'){ alert('kjl_35 error: value exists, but option is not \'set\'.'); return;}
             if (date) {
-                Cookies.set(name, value, {expires: date});
-                return;
+                if (Number.isInteger(date)) {
+                    Cookies.set(name, value, {expires: date});
+                    return;
+                }
+                else {
+                    Cookies.set(name, value, {expires: parseInt(date)});
+                    return;
+                }
             }
             else {
                 Cookies.set(name, value);
---
+++ readme.md
@@ -0,0 +1,51 @@
+[KJL_](https://iq-tree.github.io/KJL_) — New Short, Easy JavaScrpt
+==================================================
+
+What is KJL?
+--------------------------------------
+KJL_ is a JavaScript Library. It has shortened JavaScript a lot. It helps developers to write JavaScript fast.
+
+
+Documentation
+--------------------------------------
+To use KJL_, you can use constant "_". While using KJL, you won't be able to make a variable with name _.
+
+To use KJL_, you can also use kjlobject. This is also a constant.
+
+<h2>Changing CSS Style</h2>
+```
+_.css("#button", "display", "none");
+```
+CSS Style changing is a function of JavaScript minified.
+So, if you want to change `z-index`, then you will have to type it as `zIndex`.
+The first argument, `#button` could be a class element `.button` which will all change elements with that class. It also could be a tag name like `h2`. In this case, it would be the same as class element. It will change all elements with that tag name.
+
+<h2>Setting/Getting/Deleting Cookies</h2>
+```
+_.cookie("set", "myCookie", "value", 3);
+```
+For, Setting/Getting/Deleting Cookies, we used [js-cookie](https://github.com/js-cookie/js-cookie) library.
+Thanks to that, you can simply enter `"set"`, `"get"`, `"delete"` one of three and if it is a `set`, you will enter all 4 arguments and if it is one of `get` or `delete`, you can only enter 2 arguments.
+The 4th argument is the date argument. You can either type it as a Number or String.
+For the example on the top, it will create a cookie named `myCookie` with value `value`. The cookie will last for 3 days.
+If you don't enter the date, it will set as 'delete when close tab'
+
+<h2>Return element with ID</h2>
+```
+_.id("button");
+```
+This will simply return element with that id.
+
+
+
+Questions
+--------------------------------------
+For questions, contact [here]("mailto:desbey7@naver.com")
+
+
+Reminder
+--------------------------------------
+This project is not released or completed yet.
+It is just on alpha stage.
+Contact to the link on type for bug reports.
+Thank you.
